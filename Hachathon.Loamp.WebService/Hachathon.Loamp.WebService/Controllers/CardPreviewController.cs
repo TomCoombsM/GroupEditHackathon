a0bd9b47ab7b04b9759bbf5f27defc8f8c65f5ae
tom coombs <tom@moonpig.com>
2014-04-02 18:12:39 +0100

    add simple image render example

---
+++ Hachathon.Loamp.WebService/Hachathon.Loamp.WebService/Controllers/CardPreviewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Hosting;
+using System.Web.Http;
+using Hackathon.Loamp.ImageRender;
+
+namespace Hachathon.Loamp.WebService.Controllers
+{
+    public class CardPreviewController : ApiController
+    {
+        // GET api/values
+        public HttpResponseMessage Get()
+        {
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+    
+            //MemoryStream memoryStream = new MemoryStream();
+            //image.Save(memoryStream, ImageFormat.Jpeg);
+            result.Content = new ByteArrayContent(ImageRender.CreateGridImage());
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+
+            return result;
+        }
+
+       
+    }
+}
---
+++ Hackathon.Loamp.ImageRender/ImageRender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.Loamp.ImageRender
+{
+    public class ImageRender
+    {
+        public static byte[] CreateGridImage()
+        {
+            // temp - will get from db values passed in
+            System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            Stream myStream = myAssembly.GetManifestResourceStream("Hackathon.Loamp.ImageRender.image.jpg");
+
+            var image = Image.FromStream(myStream);
+  
+
+            var width = 2100;
+            var height = 2970;
+            using (var bmp = new System.Drawing.Bitmap(width, height))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(Color.Bisque);
+                    
+     
+                    Matrix mx = new Matrix();
+
+                    mx.RotateAt(40, new Point(400, 200), MatrixOrder.Append);
+
+                    g.Transform = mx;
+
+                    g.DrawImage(image, new Point(400, 200));
+
+                    g.ResetTransform();
+
+                }
+
+                var memStream = new MemoryStream();
+                bmp.Save(memStream, ImageFormat.Jpeg);
+                return memStream.ToArray();
+            }
+        }
+    }
+}
